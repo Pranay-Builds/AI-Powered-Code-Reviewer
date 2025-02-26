1d31dde340772372463710c8b75b01d766bf58ac
Pranay-Builds <contactpranay10@gmail.com>
2025-02-26 16:05:08 +0530

    Fixed some API errors and moved dotenv.config to the top to avoid any more errors

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -8,7 +8,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 async function generateContent(prompt) {
 
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
+    return result.response.text();
 }
 
 
