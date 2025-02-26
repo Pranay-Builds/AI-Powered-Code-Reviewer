d037e8065945b8a830d2cb5be17bd3784694555a
Pranay-Builds <contactpranay10@gmail.com>
2025-02-26 15:45:03 +0530

    Added AI Routes for the app using Gemini 2.0 Flash API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
+const dotenv = require('dotenv');
 
-
+dotenv.config();
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000")
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const aiRoutes = require("./routes/ai.routes");
 
 const app = express() // creates a server but does not start
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/ai', aiRoutes);
 
 module.exports = app;
 
